1a55a9369381b54e75fd9bc0238039c07c428f30
Vishal Binadi <vishalbinadi007@gmail.com>
2022-03-03 11:53:38 +0530

    Add files via upload

---
+++ costcheck.py
@@ -0,0 +1,9 @@
+cost=0.51
+costPerDay=0.51*24
+costPerWeek=costPerDay*7
+costPerMonth=costPerWeek*4
+checkCost=918/costPerDay
+print("Cost of server per day:-",costPerDay,"$")
+print("Cost of server per week:-",costPerWeek,"$")
+print("Cost of server per months:-",costPerMonth,"$")
+print("Number of days server can operate with 918$:-",checkCost)
---
+++ distance.py
@@ -0,0 +1,16 @@
+distance=int(input("How far would you like to travel in miles?"))
+
+
+if distance<3:
+     
+    print("I suggest Bicycle to your destination")
+
+
+elif distance>=3 and distance<300:
+
+    print("I suggest Motor-Cycle to your destination")
+
+
+else:
+
+    print("I suggest Supper-Car to your destination")
